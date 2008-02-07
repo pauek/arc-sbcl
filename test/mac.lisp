a5ee06987499b48451789d3cb98b47432b8cfd86
Pau Fernández <pau@urgell.(none)>
2008-02-07 08:28:35 +0100

    Separated the base walker from the compiler (wif). Added some tests for macros.

--- test/mac.lisp
+++ test/mac.lisp
@@ -17,6 +17,14 @@
 	 ,@(mapcar #'_off macs)))))
 
 (deftest mexp-1 
-  (with-mac ((do (&rest body)
-	       `(progn ,@body)))
+  (with-mac ((do (&rest b) `(progn ,@b))
+	     (x  () 'y)
+	     (z  () 5)
+	     (o  (&rest r) `(oh! ,@r)))
+    (chkmac '(fn (x) (+ x 1)) "(fn (x) (+ x 1))")
+    (chkmac '(fn (x y) (+ x y)) "(fn (x y) (+ x (x)))")
+    (chkmac '(fn (x y) (+ x y)) "(fn (x y) (+ x (x)))")
+    (chkmac '(fn ((o x 5)) (+ x 1)) "(fn ((o x (z))) (+ x 1))")
+    (chkmac '(fn ((o x -1)) (+ x 5)) "(fn ((o x -1)) (+ x 5))")
+    (chkmac '(fn ((o i (oh! 1 2))) i) "(fn ((o i (o 1 2))) i)")
     (chkmac '(progn a b) "(do a b)")))

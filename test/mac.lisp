f50742d8786ac288142c0796e5886e09b12b57f5
Pau Fernández <pau@urgell.(none)>
2008-02-07 21:46:04 +0100

    Revealed a bug in nested macroexpansion

--- test/mac.lisp
+++ test/mac.lisp
@@ -21,9 +21,14 @@
 	     (x  () 'y)
 	     (z  () 5)
 	     (o  (&rest r) `(oh! ,@r)))
+    (chkmac 'x "x")
     (chkmac '(fn (x) (+ x 1)) "(fn (x) (+ x 1))")
     (chkmac '(fn (x y) (+ x y)) "(fn (x y) (+ x (x)))")
     (chkmac '(fn ((o x 5)) (+ x 1)) "(fn ((o x (z))) (+ x 1))")
     (chkmac '(fn ((o x -1)) (+ x 5)) "(fn ((o x -1)) (+ x 5))")
     (chkmac '(fn ((o i (oh! 1 2))) i) "(fn ((o i (o 1 2))) i)")
     (chkmac '(fn (a b) (progn a b)) "(fn (a b) (do a b))")))
+
+(deftest m-nested
+  (with-mac ((do (&rest b) `(progn ,@b)))
+    (chkev 5 "(do 1 (do 3 5))")))
